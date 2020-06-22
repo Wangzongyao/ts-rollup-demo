db27886fd334d46988155d128ba0098c0c422a39
WangZongYao <wangzongyao@sunlands.com>
2020-06-22 16:36:47 +0800

    add iife

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,36 @@
 import path from 'path';
 import RollupPluginBabel from '@rollup/plugin-babel'
+import RollupPluginReplace from '@rollup/plugin-replace'
 import RollupPluginCommonJs from '@rollup/plugin-commonjs'
 import RollupPluginTypescript from '@rollup/plugin-typescript'
 import RollupPluginNodeResolve from '@rollup/plugin-node-resolve'
+
 
 export default {
     input: path.join(__dirname, './src/index.tsx'),
     output: {
         file: path.join(__dirname, './dist/bundle.js'),
-        format: 'cjs'
+        format: 'iife',
+        name: 'iife'
     },
     plugins: [
         RollupPluginNodeResolve({ 
             browser: true,
         }),
         RollupPluginCommonJs({ 
-            // extensions: ['.js', '.ts'] 
+            extensions: ['.js', '.ts', '.tsx'] 
         }),
         RollupPluginBabel({
-            runtimeHelpers: true,
             exclude: ['node_modules/**'],
+            babelHelpers: 'runtime',
         }),
         RollupPluginTypescript({ 
             module: 'commonjs',
             target: 'es5',
-            lib: ['es5', 'es6'],
+            lib: ['es5', 'es6', 'dom'],
         }),
+        RollupPluginReplace({
+            "process.env.NODE_ENV": true,
+        })
     ]
 }
